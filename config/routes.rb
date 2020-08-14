1ab2d41420bf3c8d3e3829a6521ff2f302a2e594
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-08-14 16:07:36 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ root to: "cocktails#index"
   resources :cocktails, only:[:index, :show, :new, :create, :destroy] do 
     resources :doses, only:[:new, :create]
   end 
-  resources :doses, only:[:destroy]# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :doses, only: :destroy # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
